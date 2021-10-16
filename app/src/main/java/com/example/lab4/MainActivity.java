c7ea3716a53b8a0fdc27d90ed45f8852642ac706
talejalilov <talejalilov@gmail.com>
2021-10-16 17:46:32 +0300

    NewSearchView

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,19 +2,21 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 import com.example.lab4.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity  {
 
     ActivityMainBinding  binding;
     ArrayAdapter<String> adapter;
     String[] nameList ;
-
+    ArrayList<String> arrayList;
 
 
     @Override
@@ -23,50 +25,63 @@ public class MainActivity extends AppCompatActivity  {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //I write names in res/values/strings.xml in there I create array and input name in here and then call
-        Resources resources = getResources();
-        nameList = resources.getStringArray(R.array.name_list);
-
-
-
-        //simple_list_item_multiple_choice -- this is ready code, this code create us checkbox view
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice,nameList);
-        binding.listview.setAdapter(adapter);
+        array_column();
 
         //Click listview and getPosition show on Toast message
-        binding.printSelectedItems.setOnClickListener(view -> {
-            StringBuilder itemSelected = new StringBuilder("Selected items are: \n");
-            for (int i = 0; i < binding.listview.getCount(); i++) {
-                if (binding.listview.isItemChecked(i)) {
-                    itemSelected.append(binding.listview.getItemAtPosition(i)).append("\n");
-                }
-            }
-            Toast.makeText(getApplicationContext(), itemSelected.toString(), Toast.LENGTH_SHORT).show();
-        });
+        binding.printSelectedItems.setOnClickListener(view -> buttonToPrint());
 
         //search name list in listview
         binding.searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                return true;
+                return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 adapter.getFilter().filter(newText);
-                if(adapter.getCount()<=0){
-                    binding.noRecords.setVisibility(View.VISIBLE);
-                    binding.listview.setVisibility(View.INVISIBLE);
+
+               if(adapter.isEmpty()) {
+                        binding.listview.setVisibility(View.INVISIBLE);
+                        binding.noRecords.setVisibility(View.VISIBLE);
                 }
                 else {
                     binding.listview.setVisibility(View.VISIBLE);
                     binding.noRecords.setVisibility(View.INVISIBLE);
                 }
-                return false;
+                adapter.notifyDataSetChanged();
+
+
+                return true;
             }
 
 
         });
     }
+
+    private void array_column(){
+        //I write names in res/values/strings.xml in there I create array and input name in here and then call
+        nameList = getResources().getStringArray(R.array.name_list);
+        arrayList = new ArrayList<>(Arrays.asList(nameList));
+
+
+
+        //simple_list_item_multiple_choice -- this is ready code, this code create us checkbox view
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_checked,arrayList);
+        binding.listview.setAdapter(adapter);
+
+    }
+
+    private void buttonToPrint(){
+        StringBuilder itemSelected = new StringBuilder("Selected items are: \n");
+        for (int i = 0; i < binding.listview.getCount(); i++) {
+            if (binding.listview.isItemChecked(i)) {
+                itemSelected.append(binding.listview.getItemAtPosition(i)).append("\n");
+            }
+        }
+        Toast.makeText(getApplicationContext(), itemSelected.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+
 }
 
